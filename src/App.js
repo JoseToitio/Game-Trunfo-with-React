23484fa945c738fc4999703a991fe72158aa96db
Jose <josetoitio@gmail.com>
2021-11-05 18:13:05 -0300

    exercicio 5 completo

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: Number,
-      cardAttr2: Number,
-      cardAttr3: Number,
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
     };
     this.onInputChange = this.onInputChange.bind(this);
+    this.isSaveButtonDisabled = this.isSaveButtonDisabled.bind(this);
   }
 
   onInputChange({ target }) {
@@ -25,6 +26,26 @@ class App extends React.Component {
     this.setState({
       [name]: value,
     });
+  }
+
+  isSaveButtonDisabled() {
+    const { cardName,
+      cardDescription,
+      cardImage, cardRare, cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const sum = parseInt(cardAttr1, 10)
+    + parseInt(cardAttr2, 10) + parseInt(cardAttr3, 10);
+    const limit = 90;
+    const maxLimit = 210;
+    console.log(sum);
+    if (cardName.length < 1
+    || cardDescription.length < 1
+    || cardImage.length < 1
+    || cardRare === ''
+    ) return true;
+    if (cardAttr1 > limit
+      || cardAttr2 > limit || cardAttr3 > limit || sum > maxLimit) return true;
+    if (cardAttr1 < 0 || cardAttr2 < 0 || cardAttr3 < 0) return true;
+    return false;
   }
 
   render() {
@@ -51,6 +72,7 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ this.isSaveButtonDisabled() }
         />
         <Card
           cardName={ cardName }
